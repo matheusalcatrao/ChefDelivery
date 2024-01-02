ae890de2aff3920155ab174bbc253c3430bc9aa5
matheusalcatrao@hotmail.com <matheusalcatrao@hotmail.com>
2024-01-02 18:11:02 -0300

    Initial Commit

---
+++ ChefDelivery/ChefDeliveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChefDeliveryApp.swift
+//  ChefDelivery
+//
+//  Created by Matheus Cepil Alcatrao on 02/01/24.
+//
+
+import SwiftUI
+
+@main
+struct ChefDeliveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
